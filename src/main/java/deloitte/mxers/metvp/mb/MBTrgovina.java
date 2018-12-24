2bfed9be4f003a955364335122835434926b2446
vurosevic <35973264+vurosevic@users.noreply.github.com>
2018-12-24 10:23:28 +0100

    trgovina debalans - sredjeno

---
+++ src/main/java/deloitte/mxers/metvp/mb/MBTrgovina.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package deloitte.mxers.metvp.mb;
+
+import deloitte.mxers.metvp.domen.Trgovina;
+import deloitte.mxers.metvp.lazyViews.LazyDataModelTrgovina;
+import deloitte.mxers.metvp.service.TrgovinaService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.SessionScoped;
+import org.primefaces.model.LazyDataModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ *
+ * @author Vladimir
+ */
+@Component("MBTrgovina")
+@SessionScoped
+public class MBTrgovina {
+  
+    @Autowired  
+    private TrgovinaService trgovinaService;    
+    private List<Trgovina> lista;    
+    private Trgovina selectTrgovina;
+    private Trgovina novaTrgovina;    
+    
+    private LazyDataModel<Trgovina> lazyModel;      
+
+    public TrgovinaService getTrgovinaService() {
+        return trgovinaService;
+    }
+
+    public void setTrgovinaService(TrgovinaService trgovinaService) {
+        this.trgovinaService = trgovinaService;
+    }
+
+    public List<Trgovina> getLista() {
+        return lista;
+    }
+
+    public void setLista(List<Trgovina> lista) {
+        this.lista = lista;
+    }
+
+    public Trgovina getSelectTrgovina() {
+        return selectTrgovina;
+    }
+
+    public void setSelectTrgovina(Trgovina selectTrgovina) {
+        this.selectTrgovina = selectTrgovina;
+    }
+
+    public Trgovina getNovaTrgovina() {
+        return novaTrgovina;
+    }
+
+    public void setNovaTrgovina(Trgovina novaTrgovina) {
+        this.novaTrgovina = novaTrgovina;
+    }
+
+    public LazyDataModel<Trgovina> getLazyModel() {
+        return lazyModel;
+    }
+
+    public void setLazyModel(LazyDataModel<Trgovina> lazyModel) {
+        this.lazyModel = lazyModel;
+    }
+    
+    @PostConstruct
+    public void init(){     
+         setLista(getTrgovinaService().findAll());         
+         setLazyModel(new LazyDataModelTrgovina(getTrgovinaService().findAll())); 
+         setNovaTrgovina(new Trgovina());
+         setSelectTrgovina(null); 
+    }       
+    
+    public void dodajNovuTrgovinu(){
+        trgovinaService.save(novaTrgovina);
+        init();
+    }
+    
+    public void azurirajTrgovinu() {
+        trgovinaService.save(selectTrgovina);
+    }
+    
+    public void brisiVrstuElektrane() {        
+        trgovinaService.delete(selectTrgovina); 
+        init();
+    }    
+}
